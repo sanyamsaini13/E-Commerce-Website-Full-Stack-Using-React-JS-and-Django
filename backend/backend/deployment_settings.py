23f9101bdc9a8ef5e2f8dd3e5c43ed487ec71057
Sanyam <sanyamsaini13@gmail.com>
2025-12-21 15:54:24 +0530

    Add backend and frontend files properly as normal folders

---
+++ backend/backend/deployment_settings.py
@@ -0,0 +1,57 @@
+import os
+import dj_database_url
+from .settings import *
+from .settings import BASE_DIR
+
+
+RENDER_HOST = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+
+ALLOWED_HOSTS = [RENDER_HOST] if RENDER_HOST else ["localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = (
+    [f"https://{RENDER_HOST}"] if RENDER_HOST else []
+)
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key-for-local")
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    "corsheaders.middleware.CorsMiddleware",   # after security
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+
+# CORS (use ONE of these)
+CORS_ALLOW_ALL_ORIGINS = True
+# OR
+# CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
+}
+
+
